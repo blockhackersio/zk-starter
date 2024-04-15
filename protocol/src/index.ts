b9bdd6ed148086bd0ac951d6fafff42b787f1820
ryardley <contact@rudiyardley.com>
2024-04-15 18:12:52 +1000

    Refactor to Multiplier

--- protocol/src/index.ts
+++ protocol/src/index.ts
@@ -1,17 +1,21 @@
 // export your SDK here
 
+import { Provider } from "ethers";
 import { Multiplier__factory } from "../typechain-types";
 import { generateGroth16Proof } from "./zklib";
 
 export class Multiplier {
-  constructor(private address: string) {}
+  constructor(
+    private provider: Provider,
+    private address: string 
+  ) { }
 
   async prove(a: number, b: number) {
     return await generateGroth16Proof({ a, b }, "multiplier");
   }
 
   async verify(proof: string, c: number) {
-    const verifier = Multiplier__factory.connect(this.address);
+    const verifier = Multiplier__factory.connect(this.address, this.provider);
     return await verifier.verify(proof, [c]);
   }
 }
--- protocol/test/Multiplier.ts
+++ protocol/test/Multiplier.ts
@@ -1,5 +1,5 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { ignition } from "hardhat";
+import { ethers, ignition } from "hardhat";
 import { expect } from "chai";
 import { Multiplier } from "../src";
 import ProtocolModule from "../ignition/modules/Protocol";
@@ -12,14 +12,16 @@ describe("Multiplier", () => {
   describe("Deployment", () => {
     it("should pass a valid proof", async () => {
       const { verifier } = await loadFixture(deployVerifierFixture);
-      const multiplier = new Multiplier(await verifier.getAddress());
+      const address = await verifier.getAddress();
+      const multiplier = new Multiplier(ethers.provider, address);
       const proof = await multiplier.prove(4, 11);
       await multiplier.verify(proof, 44);
     });
 
     it("should fail an invalid proof", async () => {
       const { verifier } = await loadFixture(deployVerifierFixture);
-      const multiplier = new Multiplier(await verifier.getAddress());
+      const address = await verifier.getAddress();
+      const multiplier = new Multiplier(ethers.provider, address);
       const proof = await multiplier.prove(4, 10);
       await expect(multiplier.verify(proof, 44)).to.be.revertedWith(
         "invalid proof"
