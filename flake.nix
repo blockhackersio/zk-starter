4f6225b86f625b27c4ae66759a0e6398235736cd
гλ <contact@rudiyardley.com>
2024-04-17 17:58:10 +1000

    Add circom-lsp for neovim support (#1)

--- flake.nix
+++ flake.nix
@@ -20,6 +20,22 @@
             solc.overlay
           ];
         };
+        circom-lsp = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "circom-lsp";
+          version = "0.1.3";  # Specify the correct version
+
+          src = pkgs.fetchFromGitHub {
+            owner = "rubydusa";  # Specify the owner
+            repo = pname;
+            rev = "v${version}";
+            sha256 = "sha256-Y71qmeDUh6MwSlFrSnG+Nr/un5szTUo27+J/HphGr7M=";  # Provide the correct hash
+          };
+
+          cargoSha256 = "sha256-Lq8SpzkUqYgayQTApNngOlhceAQAPG9Rwg1pmGvyxnM=";  # Provide the correct cargo hash
+          sourceRoot = "source/server";
+          doCheck = false;
+        };
+
       in 
       with pkgs;
       {
@@ -29,6 +45,7 @@
             circom
             solc_0_8_24
             (solc.mkDefault pkgs solc_0_8_24)
+            circom-lsp
           ];
         };
       }    
