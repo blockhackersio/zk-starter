2acd4fea759af94ea0ed4f1e78d1e86b417d02fd
ryardley <contact@rudiyardley.com>
2024-04-18 21:09:23 +1000

    Tidy up frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,60 +2,77 @@ import { FormEvent, useState } from "react";
 import "./App.css";
 import { CircomExample, getAddress } from "@blockhackers/protocol";
 import { JsonRpcProvider } from "ethers";
+
 const provider = new JsonRpcProvider();
+const example = new CircomExample(
+  provider,
+  getAddress("chain-31337", "CircomExample")
+);
+
 function App() {
   const [status, setStatus] = useState<"init" | "error" | "success">("init");
+  const [proof, setProof] = useState<string>("");
+  const [provingTime, setProvingTime] = useState<number | undefined>();
+  const [verifyingTime, setVerifyingTime] = useState<number | undefined>();
+
   async function onSubmit(e: FormEvent<HTMLFormElement>) {
     setStatus("init");
+    setProof("");
     e.preventDefault();
+
     const data = new FormData(e.currentTarget);
     const a = Number(data.get("a"));
     const b = Number(data.get("b"));
     const c = Number(data.get("c"));
-    console.log({ a, b, c });
-    const example = new CircomExample(
-      provider,
-      getAddress("chain-31337", "CircomExample")
-    );
+    let time = new Date();
     const proof = await example.multiplierProve(a, b);
+    setProvingTime(Number(new Date()) - Number(time));
+    setProof(proof);
+    time = new Date();
     try {
       await example.multiplierVerify(proof, c);
       setStatus("success");
     } catch (err) {
       setStatus("error");
     }
+    setVerifyingTime(Number(new Date()) - Number(time));
   }
 
   return (
-    <form
-      onSubmit={onSubmit}
-      style={{ display: "flex", flexDirection: "column", gap: "10px" }}
-    >
-      <div style={{ display: "flex", gap: "10px" }}>
-        <label>a: </label>
-        <input name="a" />
-      </div>
-      <div style={{ display: "flex", gap: "10px" }}>
-        <label>b: </label>
-        <input name="b" />
-      </div>
-      <div style={{ display: "flex", gap: "10px" }}>
-        <label>c: </label>
-        <input name="c" />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          gap: "10px",
-        }}
-      >
-        <button>Prove and verify</button>
-        {status === "error" && <div style={{ color: "red" }}>×</div>}
-        {status === "success" && <div style={{ color: "green" }}>✓</div>}
-      </div>
-    </form>
+    <div>
+      <h2>Prove and Verify your zkSNARK</h2>
+      <blockquote>a x b = c</blockquote>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="a">
+          a:
+          <input type="number" name="a" />
+        </label>
+        <label>
+          b:
+          <input type="number" name="b" />
+        </label>
+        <label>
+          c:
+          <input type="number" name="c" />
+        </label>
+        {status === "init" && <button>Prove and verify</button>}
+        {status === "error" && (
+          <button>
+            Try Again <span className="error">×</span>
+          </button>
+        )}
+        {status === "success" && (
+          <button>
+            Proof is valid <span className="success">✓</span>
+          </button>
+        )}
+        {provingTime !== undefined && <div>Proof took {provingTime}ms</div>}
+        {verifyingTime !== undefined && (
+          <div>Verification took {verifyingTime}ms</div>
+        )}
+      </form>
+      {proof && <pre>{proof}</pre>}
+    </div>
   );
 }
 
